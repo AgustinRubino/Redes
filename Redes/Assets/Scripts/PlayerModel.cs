33f4b7fa4c04123a837484929d5650dc37df964e
Agustín Rolón <102646605+RoloWasHere@users.noreply.github.com>
2026-06-14 13:00:51 -0300

    a

--- Redes/Assets/Scripts/PlayerModel.cs
+++ Redes/Assets/Scripts/PlayerModel.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public class PlayerModel : NetworkBehaviour
 {
+    public static PlayerModel LocalPlayer {get; private set;}
+
     [SerializeField] float _maxHP = 100;
     [SerializeField] float _currentHP = 100;
 
@@ -16,9 +18,22 @@ public class PlayerModel : NetworkBehaviour
 
     public override void Spawned()
     {
+        if (HasStateAuthority && LocalPlayer == null)
+        {
+            LocalPlayer = this;
+        }
+
         movement = GetComponent<ForceMovement>();
         if (_mesh == null) _mesh = GetComponentInChildren<MeshRenderer>();
         _mesh.material.color = MeshColor;
+    }
+
+    public override void Despawned(NetworkRunner runner, bool hasState)
+    {
+        if (HasStateAuthority && LocalPlayer == this)
+        {
+            LocalPlayer = null;
+        }
     }
 
     private void OnColorChanged()
